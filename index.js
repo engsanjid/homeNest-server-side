e743f8856ef3d3208a332131bf179bbb2404a46f
engsanjid <mdsanjidi36@gmail.com>
2025-11-09 18:07:18 +0600

    added Middleware

--- index.js
+++ index.js
@@ -7,7 +7,9 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 5000;
 
-
+// Middleware
+app.use(cors());
+app.use(express.json());
 
 // MongoDB Connection
 const uri = process.env.MONGO_URI;
